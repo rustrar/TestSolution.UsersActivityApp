f3ae81378b60f1ff0054de9231b535f3f4ea73e8
rustrar <rustrar@gmail.com>
2021-09-25 18:17:26 +0400

    Настройка CORS

--- UsersActivityApp.WebApi/Startup.cs
+++ UsersActivityApp.WebApi/Startup.cs
@@ -23,6 +23,12 @@ namespace UsersActivityApp.WebApi
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      //Enable CORS
+      services.AddCors(c =>
+      {
+        c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+      });
+
       services.AddControllers();
       services.AddDbContext<UsersActivityDbContext>(x =>
         x.UseNpgsql(Configuration.GetConnectionString("UsersActivityDbContext")));
@@ -35,6 +41,9 @@ namespace UsersActivityApp.WebApi
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+      //Enable CORS
+      app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
